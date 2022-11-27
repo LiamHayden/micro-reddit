755302c5ee46202dd58e1777ac8bb71da0a71732
Liam Hayden <l.hayden67@outlook.com>
2022-11-27 16:53:37 +0000

    Add scopes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :comments
 
+    scope :newest, -> { order(created_at: :desc) }
+    scope :oldest, -> { order(created_at: :asc) }
+
     validates :title, :content, :user_id, presence: true
     validates :title, length: { maximum: 25 }
     validates :content, length: { minimum: 26 }
